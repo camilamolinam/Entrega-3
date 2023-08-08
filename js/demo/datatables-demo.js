f685a3cadfd56de1b24fa67d99d092838c50d097
Maria Camila Molina <camilamolinam@gmail.com>
2023-08-08 18:00:53 -0500

    modifico index.html, datatables-demo.js y chhart-area-demo.js

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -2,3 +2,21 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+
+const tblValores=document.getElementById("tbValores")
+function cargartabla(){
+  axios.get("http://ucamp.alumnos.dev4humans.com.mx/Main/endpoint_ingresos_mensuales").then(resultado => {
+        console.log(resultado);
+      tblValores.innerHTML="";
+      for(const valor of resultado.data.data){
+        let tr =`<tr>
+        <td>${valor.id}</td>
+        <td>${valor.nombre}</td>
+        <td>${valor.monto}</td>
+       </tr> `
+       tblValores.innerHTML+=tr;
+      }
+
+})
+}
+cargartabla()
